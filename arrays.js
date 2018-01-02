9d960d1cc52c39d62902f1acf561f880cd220f27
Souloinc87 <souloinc87@gmail.com>
2018-01-02 06:59:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -31,7 +31,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   return array
 }
 function removeElementFromEndOfArray(array) {
-  array.slice(2, array.length + 2)
-  array = array.slice(2, array.length + 2)
+  array.slice(2, array.length - 3)
   return array
 }
