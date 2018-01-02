a1d4d16e2023e1c8f294fb659b3859ce448855a0
Souloinc87 <souloinc87@gmail.com>
2018-01-02 06:46:27 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -31,6 +31,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   return array
 }
 function removeElementFromEndOfArray(array) {
-  array.slice(0, array.length - 1)
+  array.slice(2, array.length - 1)
   return array
 }
